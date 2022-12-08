88e5c1f63fadd18f585d6c9c88aa1142f45215ff
janishub <113140850+janishub@users.noreply.github.com>
2022-12-08 09:59:42 +0100

    Add files via upload

---
+++ code/basis/empty.ino
@@ -0,0 +1,81 @@
+#include "SerialCommand.h"
+#include "EEPROMAnything.h"
+
+#define SerialPort Serial
+#define Baudrate 115200
+
+SerialCommand sCmd(SerialPort);
+bool debug;
+unsigned long previous, calculationTime;
+
+struct param_t
+{
+  unsigned long cycleTime;
+  /* andere parameters die in het eeprom geheugen moeten opgeslagen worden voeg je hier toe ... */
+} params;
+
+void setup()
+{
+  SerialPort.begin(Baudrate);
+
+  sCmd.addCommand("set", onSet);
+  sCmd.addCommand("debug", onDebug);
+  sCmd.setDefaultHandler(onUnknownCommand);
+
+  EEPROM_readAnything(0, params);
+
+  pinMode(13, OUTPUT);
+  SerialPort.println("ready");
+}
+
+void loop()
+{
+  sCmd.readSerial();
+ 
+  unsigned long current = micros();
+  if (current - previous >= params.cycleTime)
+  {
+    previous = current;
+
+    /* code die cyclisch moet uitgevoerd worden programmeer je hier ... */
+
+    /* normaliseren en interpoleren sensor */
+
+    /* pid regeling */
+
+    /* aansturen motoren */
+  }
+
+  unsigned long difference = micros() - current;
+  if (difference > calculationTime) calculationTime = difference;
+}
+
+void onUnknownCommand(char *command)
+{
+  SerialPort.print("unknown command: \"");
+  SerialPort.print(command);
+  SerialPort.println("\"");
+}
+
+void onSet()
+{
+  char* param = sCmd.next();
+  char* value = sCmd.next();  
+  
+  if (strcmp(param, "cycle") == 0) params.cycleTime = atol(value);
+  /* parameters een nieuwe waarde geven via het set commando doe je hier ... */
+  
+  EEPROM_writeAnything(0, params);
+}
+
+void onDebug()
+{
+  SerialPort.print("cycle time: ");
+  SerialPort.println(params.cycleTime);
+  
+  /* parameters weergeven met behulp van het debug commando doe je hier ... */
+  
+  SerialPort.print("calculation time: ");
+  SerialPort.println(calculationTime);
+  calculationTime = 0;
+}
